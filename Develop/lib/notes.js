d8e33320062dbc364954ed4e6b05a1572ef8ec20
brt9029 <brt9029@gmail.com>
2021-12-18 13:39:14 -0600

    functionality completed except for DELETE requests

--- Develop/lib/notes.js
+++ Develop/lib/notes.js
@@ -9,8 +9,14 @@ function createNewNote(body, notesArray) {
         JSON.stringify({ notes: notesArray }, null, 2)
     );
     return note;
-}
+};
+
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+};
 
 module.exports = {
-    createNewNote
+    createNewNote,
+    findById
 }
