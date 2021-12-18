b32008762118d33151e12ac78f1a77bcdd34cce8
brt9029 <brt9029@gmail.com>
2021-12-17 22:19:36 -0600

    updated routing

--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -1,25 +1,8 @@
 const router = require('express').Router();
-const { notes } = require('../../db/notes');
+const { notes } = require('../../db/db');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
-
-router.get('/notes/:id', (req, res) => {
-    const result = findById(req.params.id, zookeepers);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-});
-
-router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-
-    const note = createNewNote(req.body, notes);
-    res.join(note);
-});
-
 
 module.exports = router;
