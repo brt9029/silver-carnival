e81122154da19348f8d0588a8603f61b1f2f4cf6
brt9029 <brt9029@gmail.com>
2021-12-18 12:40:34 -0600

    added unique id package for additional notes

--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db');
 const { createNewNote } = require('../../lib/notes');
+const uniqid = require('uniqid');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
+    req.body.id = uniqid();
     const note = createNewNote(req.body, notes);
     res.json(note);
 });
--- Develop/server.js
+++ Develop/server.js
@@ -17,5 +17,5 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`)
+    console.log(`API server now on port ${PORT}!`);
 });
