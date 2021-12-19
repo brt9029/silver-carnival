87b2569b06fc0cf3f8a03f206eab94595d93d836
brt9029 <brt9029@gmail.com>
2021-12-19 16:32:44 -0600

    updating folder directory structure

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,15 +1,10 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db');
-const { createNewNote, findById, removeNote } = require('../../lib/notes');
+const { createNewNote } = require('../../lib/notes');
 const uniqid = require('uniqid');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
-});
-
-router.delete('/notes/:id', (req, res) => {
-    deleteNote(findById(req.params.id, notes));
-    res.send('Note deleted!');
 });
 
 router.post('/notes', (req, res) => {
